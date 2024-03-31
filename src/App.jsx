252c6911738ac286b107717156168b8b69518c7f
Erdoğan FIRAT <efirat3@gmail.com>
2024-03-31 14:21:34 +0300

    Kim milyoner olmak ister react uygulaması

---
+++ src/App.jsx
@@ -0,0 +1,156 @@
+import { useEffect, useMemo, useState } from "react";
+import "./app.css";
+import Trivia from "./components/Trivia";
+import Timer from "./components/Timer";
+import Start from "./components/Start";
+
+function App() {
+  const [username, setUsername] = useState("Erdoğan");
+  const [questionNumber, setQuestionNumber] = useState(1);
+  const [stop, setStop] = useState(false);
+  const [earned, setEarned] = useState("0 TL");
+
+  const data = [
+    {
+      id: 1,
+      question: "Rolex is a company that specializes in what type of product?",
+      answers: [
+        {
+          text: "Phone",
+          correct: false,
+        },
+        {
+          text: "Watches",
+          correct: true,
+        },
+        {
+          text: "Food",
+          correct: false,
+        },
+        {
+          text: "Cosmetic",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 2,
+      question: "When did the website `Facebook` launch?",
+      answers: [
+        {
+          text: "2004",
+          correct: true,
+        },
+        {
+          text: "2005",
+          correct: false,
+        },
+        {
+          text: "2006",
+          correct: false,
+        },
+        {
+          text: "2007",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "Who played the character of harry potter in movie?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+  ];
+
+  const moneyPyramid = useMemo(
+    () =>
+      [
+        { id: 1, amount: "2.000 TL" },
+        { id: 2, amount: "5.000 TL" },
+        { id: 3, amount: "7.500 TL" },
+        { id: 4, amount: "10.000 TL" },
+        { id: 5, amount: "20.000 TL" },
+        { id: 6, amount: "30.000 TL" },
+        { id: 7, amount: "50.000 TL" },
+        { id: 8, amount: "100.000 TL" },
+        { id: 9, amount: "200.000 TL" },
+        { id: 10, amount: "300.000 TL" },
+        { id: 11, amount: "500.000 TL" },
+        { id: 12, amount: "1.000.000 TL" },
+        { id: 13, amount: "5.000.000 TL" },
+      ].reverse(),
+    []
+  );
+
+  useEffect(() => {
+    questionNumber > 1 &&
+      setEarned(moneyPyramid.find((m) => m.id === questionNumber - 1).amount);
+  }, [moneyPyramid, questionNumber]);
+
+  return (
+    <div className="app">
+      {username ? (
+        <>
+          <div className="main">
+            {stop ? (
+              <h1 className="endText">You earned : {earned}</h1>
+            ) : (
+              <>
+                <div className="top">
+                  <div className="timer">
+                    <Timer setStop={setStop} questionNumber={questionNumber} />
+                  </div>
+                </div>
+                <div className="bottom">
+                  <Trivia
+                    data={data}
+                    setStop={setStop}
+                    questionNumber={questionNumber}
+                    setQuestionNumber={setQuestionNumber}
+                  />
+                </div>
+              </>
+            )}
+          </div>
+          <div className="pyramid">
+            <ul className="moneyList">
+              {moneyPyramid.map((m) => (
+                <li
+                  className={
+                    questionNumber === m.id
+                      ? "moneyListItem active"
+                      : "moneyListItem"
+                  }
+                >
+                  <span className="monetListItemNumber">{m.id}</span>
+                  <span className="monetListItemAmount">{m.amount}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      ) : (
+        <Start setUsername={setUsername} />
+      )}
+    </div>
+  );
+}
+
+export default App;
